83d9faa3b6a4f7c5da1e340b5941aa1f9370560d
Balasubramanian Muthuvelu <balasubhramanian@gmail.com>
2017-12-07 23:08:16 +0530

    React UI - create-react app

---
+++ app/ui/donation-ui/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Donation App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
