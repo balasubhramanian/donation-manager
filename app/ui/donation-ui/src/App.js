c1abb2f4cf8d8646fdd685b0dc649d3d2d11f6b0
Balasubramanian Muthuvelu <balasubhramanian@gmail.com>
2017-12-11 21:49:46 +0530

    Added Font awesome

--- app/ui/donation-ui/src/App.js
+++ app/ui/donation-ui/src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.css'
-
+import 'font-awesome/css/font-awesome.css'
 import LoginPage from "pages/login"
+import ListUserPage from "pages/user/list-user"
 
 class App extends Component {
   render() {
