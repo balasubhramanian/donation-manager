be84130fb81a6f4dad4b7f2fa93aa2626b607e99
Balasubramanian Muthuvelu <balasubhramanian@gmail.com>
2017-12-14 19:24:27 +0530

    Added redux provider

--- app/ui/donation-ui/src/index.js
+++ app/ui/donation-ui/src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import store from "./store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./App";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
