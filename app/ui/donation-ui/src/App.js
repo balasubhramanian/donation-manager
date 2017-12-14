3c6f684b6ed54f2f566bfea7b0093076f0d64c8b
Balasubramanian Muthuvelu <balasubhramanian@gmail.com>
2017-12-14 19:26:25 +0530

    added react-router with basic redux & login support

--- app/ui/donation-ui/src/App.js
+++ app/ui/donation-ui/src/App.js
@@ -1,39 +1,42 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { history } from "./store";
+import { ConnectedRouter } from "react-router-redux";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
 import LoginPage from "pages/login";
+import AppLayout from "layout/left-nav-layout";
 import ListUserPage from "pages/user/list-user";
 
-class EnsureLoggedInContainer extends Component {
-  componentDidMount() {
-    if (!window.isLoggedIn) {
-      this.props.history.replace("/login");
-    }
+const ProtectedContainer = props => {
+  if (!props.isLoggedIn) {
+    history.push("/login");
   }
+  return (
+    <AppLayout>
+      <Route path="/user" component={ListUserPage} />
+    </AppLayout>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        {/* User Routes */}
-        <Route path="/user" component={ListUserPage} />
-      </div>
-    );
-  }
-}
+const ConnectedProtectedContainer = connect(state => ({ ...state.auth }), {})(
+  ProtectedContainer
+);
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Route exact path="/" component={LoginPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route component={EnsureLoggedInContainer} />
-        </div>
-      </BrowserRouter>
+      <div>
+        <ConnectedRouter history={history}>
+          <Switch>
+            <Route path="/login" component={LoginPage} />
+            <Route path="/" component={ConnectedProtectedContainer} />
+          </Switch>
+        </ConnectedRouter>
+      </div>
     );
   }
 }
