8d540bd9d82f04a6f96a1e4255c3ea57724d4a31
Balasubramanian Muthuvelu <balasubhramanian@gmail.com>
2017-12-14 19:24:56 +0530

    removed layout, will be handled as part of routes

--- app/ui/donation-ui/src/pages/user/list-user.js
+++ app/ui/donation-ui/src/pages/user/list-user.js
@@ -4,9 +4,7 @@ import AppLayout from "layout/left-nav-layout";
 export default class ListUser extends Component {
   render() {
     return (
-      <AppLayout>
         <h1>List All Users</h1>
-      </AppLayout>
     );
   }
 }
