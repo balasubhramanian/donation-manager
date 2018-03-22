07e85c9a24bae0e6692997df3495aaa88a594be5
Balasubramanian Muthuvelu <balasubhramanian@gmail.com>
2018-03-22 21:58:43 +0530

    Init  Spring boot with Mysql

---
+++ app/api/donation-api/src/main/java/com/bala/donation/config/WebConfiguration.java
@@ -0,0 +1,33 @@
+package com.bala.donation.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebConfiguration {
+    /*
+     * @Bean ServletRegistrationBean h2servletRegistration() {
+     * ServletRegistrationBean registrationBean = new ServletRegistrationBean(new
+     * WebServlet()); registrationBean.addUrlMappings("/console/*"); return
+     * registrationBean; }
+     */
+
+    // CORS Filter
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setOrder(0);
+        return new CorsFilter(source);
+    }
+}
