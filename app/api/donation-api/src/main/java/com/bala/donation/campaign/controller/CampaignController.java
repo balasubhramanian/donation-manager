09658a84b8755f46d37310ffb7badc07bfee12bc
Balasubramanian Muthuvelu <balasubhramanian@gmail.com>
2018-09-21 23:06:57 +0530

    export campaign

--- app/api/donation-api/src/main/java/com/bala/donation/campaign/controller/CampaignController.java
+++ app/api/donation-api/src/main/java/com/bala/donation/campaign/controller/CampaignController.java
@@ -1,7 +1,10 @@
 package com.bala.donation.campaign.controller;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.supercsv.cellprocessor.Optional;
+import org.supercsv.cellprocessor.constraint.NotNull;
+import org.supercsv.cellprocessor.constraint.Unique;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
 
 import com.bala.donation.campaign.model.CampaignModel;
 import com.bala.donation.campaign.service.CampaignService;
@@ -28,6 +37,43 @@ public class CampaignController {
             @RequestParam(value = "is_active", defaultValue = "false") boolean onlyActive) {
         List<CampaignModel> campaigns = campaignService.getAllCampaign(onlyActive);
         return ResponseEntity.ok(campaigns);
+    }
+
+    @RequestMapping(path = "/campaign/export/csv", method = RequestMethod.GET)
+    public @ResponseBody byte[] exportAllCampaignAsCSV(
+            @RequestParam(value = "is_active", defaultValue = "false") boolean onlyActive,
+            HttpServletResponse response) {
+        List<CampaignModel> campaigns = campaignService.getAllCampaign(onlyActive);
+        String[] header = new String[] { "id", "name", "type", "startDate", "endDate" };
+
+        final CellProcessor[] processors = new CellProcessor[] { new Unique(), // id
+                new NotNull(), // name
+                new Optional(), // type
+                new Optional(), // startDate
+                new Optional() // endDate
+
+        };
+        try (StringWriter strWriter = new StringWriter();
+                CsvBeanWriter beanWriter = new CsvBeanWriter(strWriter, CsvPreference.STANDARD_PREFERENCE)) {
+            beanWriter.writeHeader(header);
+
+            for (CampaignModel donors : campaigns) {
+                beanWriter.write(donors, header, processors);
+            }
+            beanWriter.close();
+            strWriter.close();
+
+            response.setContentType("application/csv");
+            response.addHeader("Content-Disposition", "attachment; filename=" + "campaign.csv");
+
+            return strWriter.toString().getBytes();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
     @RequestMapping(path = "/campaign", method = RequestMethod.POST)
